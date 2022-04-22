25eb4d0d1e88d6be4ba819f23ff3830a1527bf67
Beldine-Moturi <beldinemoturi@gmail.com>
2022-04-22 17:25:49 +0300

    try different way

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
-"""generates a .tgz archive from the contents of the web_static folder
-in the current working directory"""
+'''
+fabric script to distribute an archive to web servers
+----NEEDS TO REVISIT SCRIPT
+'''
+
+import os
 from datetime import datetime
-from fabric.api import local, put, run, cd
-import os.path
+from fabric.api import env, local, put, run, runs_once
 
 
-def do_pack():
+env.hosts = ['34.148.201.37', '34.204.181.101']
+
+
+def do_deploy(archive_path):
     """Distributes an archive to a web server.
     Args:
         archive_path (str): The path of the archive to distribute.
@@ -14,44 +20,49 @@ def do_pack():
         If the file doesn't exist at archive_path or an error occurs - False.
         Otherwise - True.
     """
-
-    local("mkdir -p versions")
-    path = "versions/web_static_{}.tgz".format(
-        datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    cur_time = datetime.now()
+    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        cur_time.year,
+        cur_time.month,
+        cur_time.day,
+        cur_time.hour,
+        cur_time.minute,
+        cur_time.second
     )
-    result = local("tar -cvzf {} ./web_static/".format(path))
-    if result.failed:
-        return None
-    return path
+    try:
+        print("Packing web_static to {}".format(output))
+        local("tar -cvzf {} web_static".format(output))
+        archize_size = os.stat(output).st_size
+        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
+    except Exception:
+        output = None
+    return output
 
 
-def do_deploy(arhive_path):
+def do_deploy(archive_path):
     """Deploys the static files to the host servers.
     Args:
         archive_path (str): The path to the archived static files.
     """
-
-    # env.user = "ubuntu"
-    env.hosts = ['34.148.201.37'. '34.204.181.101']
-    # env.key_filename = "~/.ssh/ssh_key"
-
     if not os.path.exists(archive_path):
         return False
-    # do_pack()
-    archive_file = os.path.basename(archive_path)
-    folder_name = archive_file.replace(".tgz", "")
+    file_name = os.path.basename(archive_path)
+    folder_name = file_name.replace(".tgz", "")
     folder_path = "/data/web_static/releases/{}/".format(folder_name)
-
+    success = False
     try:
-        put("archive_path", "/tmp/{}".format(archive_file))
+        put(archive_path, "/tmp/{}".format(file_name))
         run("mkdir -p {}".format(folder_path))
-        run("tar -xzf /tmp/{} -C {}".format(archive_file, folder_path))
-        run("rm -rf /tmp/{}".format(archive_file))
+        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
+        run("rm -rf /tmp/{}".format(file_name))
         run("mv {}web_static/* {}".format(folder_path, folder_path))
         run("rm -rf {}web_static".format(folder_path))
         run("rm -rf /data/web_static/current")
         run("ln -s {} /data/web_static/current".format(folder_path))
-        print("New version deployed!")
+        print('New version deployed!')
+        success = True
     except Exception:
-        return False
-    return True
+        success = False
+    return success
