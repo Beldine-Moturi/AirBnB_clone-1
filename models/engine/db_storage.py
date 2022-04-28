3dd25919b6debcb8a653bd5fe189170e4cbc1896
Beldine-Moturi <beldinemoturi@gmail.com>
2022-04-28 15:34:35 +0300

    [fix]: fix errors

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -35,6 +35,7 @@ class DBStorage:
         self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".format(
             HBNB_MYSQL_USER, HBNB_MYSQL_PWD, HBNB_MYSQL_HOST, HBNB_MYSQL_DB
         ), pool_pre_ping=True)
+#        self.engine = create_engine('mysql+mysqldb://'+HBNB_MYSQL_USER+':'+HBNB_MYSQL_PWD+'@'+HBNB_MYSQL_HOST+'/'+HBNB_MYSQL_DB+"?charset=utf8")
 
         if HBNB_ENV == "test":
             Base.metadata.drop_all(self.__engine)
--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -11,15 +11,19 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.teardown_appcontext
-def close_db_session():
-    """removes the current SQLAlchemy Session
-    after each request"""
-    storage.close()
-
-
 @app.route('/states_list')
 def display_states():
     """display a HTML page: (inside the tag BODY)"""
     states = storage.all(State)
     return render_template('7-states_list.html', states=states.values())
+
+
+@app.teardown_appcontext
+def close_db_session(exception):
+    """removes the current SQLAlchemy Session
+    after each request"""
+    storage.close()
+
+if __name__ == "__main__":
+    """run flask web app"""
+    app.run(host='0.0.0.0', port=5000)
